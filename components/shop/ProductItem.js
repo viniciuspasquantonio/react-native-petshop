9ff5e018043745731ff453042c9e324917eab049
Vinicius Pasquantonio <vinicius.pasquantonio@agido.com>
2020-05-06 00:20:39 +0200

    product detail

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,22 +1,33 @@
 import React from 'react';
-import {View, Text,Image,StyleSheet, Button} from 'react-native'
+import {View, Text,Image,StyleSheet, Button, TouchableOpacity, TouchableNativeFeedback, Platform} from 'react-native'
 import Colors from '../../constants/Colors';
 
 const ProductItem = props => {
+    let TouchableCmp = TouchableOpacity;
+    if(Platform.OS === 'android' && Platform.Version >= 21){
+        TouchableCmp = TouchableNativeFeedback;
+    }
     return (
         <View style={styles.product}>
-            <View style={styles.imageContainer}>
-                <Image style={styles.image} source={{uri: props.image}}/>
-            </View>
-            <View style={styles.details}> 
-                <Text color={Colors.primary} style={styles.title}>{props.title}</Text>
-                <Text color={Colors.primary} style={styles.price}>${props.price.toFixed(2)}</Text>
-            </View>
-            <View style={styles.actions}>
-                <Button color={Colors.primary} title= "View details" onPress={props.onViewDetail}/>
-                <Button color={Colors.primary} title= "To Cart" onPress={props.onAddCart}/>
+            <View style={styles.touchable}>
+                <TouchableCmp onPress={props.onViewDetail} useForeground>
+                    <View>
+                        <View style={styles.imageContainer}>
+                            <Image style={styles.image} source={{uri: props.image}}/>
+                        </View>
+                        <View style={styles.details}> 
+                            <Text color={Colors.primary} style={styles.title}>{props.title}</Text>
+                            <Text color={Colors.primary} style={styles.price}>${props.price.toFixed(2)}</Text>
+                        </View>
+                        <View style={styles.actions}>
+                            <Button color={Colors.primary} title= "View details" onPress={props.onViewDetail}/>
+                            <Button color={Colors.primary} title= "To Cart" onPress={props.onAddCart}/>
+                        </View>
+                    </View>
+                </TouchableCmp>
             </View>
         </View>
+        
     );
 };
 
@@ -31,6 +42,10 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         height: 300,
         margin: 20
+    },
+    touchable:{
+        borderRadius: 10,
+        overflow:'hidden'
     },
     imageContainer: {
         width: '100%',
---
+++ screens/shop/ProductDetailScreen.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {View, Text,Image,Button,StyleSheet} from 'react-native';
+import ProductItem from '../../components/shop/ProductItem';
+import {userSelector, useSelector} from 'react-redux';
+
+const ProductDetailScreen = props => {
+    const productId = props.navigation.getParam('productId');
+    const selectedProduct = useSelector(state => state.products.availableProducts.find(prod => prod.id === productId));
+
+
+    return (
+        <View>
+            <Text>{selectedProduct.title}</Text>
+        </View>
+    );
+};
+
+ProductDetailScreen.navigationOptions = navData => {
+    return {
+        headerTitle: navData.navigation.getParam('productTitle')
+    };
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default ProductDetailScreen;
